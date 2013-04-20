edff627953cd479c4253e02c0581e65e871026af
danielgm <dan@danielmclaren.com>
2013-04-20 16:51:11 -0700

    Initial commit.

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -14,129 +14,31 @@ void testApp::setup() {
 	
 	ofSetFrameRate(60);
 	kinect.setCameraTiltAngle(0);
-	
-	gotKinectFrame = false;
-	
-	readFrames("out12.mov");
 }
 
 void testApp::update() {
 	kinect.update();
 	if(kinect.isFrameNew()) {
 		maskOfPixels.setFromPixels(kinect.getDepthPixels(), kinect.width, kinect.height, OF_IMAGE_GRAYSCALE);
-		if (maskOfPixels.isAllocated()) {
-			maskOfPixels.resize(frameWidth, frameHeight);
-			blur(maskOfPixels, 100);
-			maskPixels = maskOfPixels.getPixels();
-			
-			// Make sure mask values are within the expected range.
-			for (int i = 0; i < frameWidth * frameHeight; i++) {
-				maskPixels[i] = MIN(maskPixels[i], 255);
-				maskPixelsDetail[i] = maskPixels[i] * 255;
-			}
-			gotKinectFrame = true;
-		}
+		blur(maskOfPixels, 100);
 	}
 }
 
 void testApp::draw() {
 	ofBackground(0);
+	ofSetColor(255, 255, 255);
 	
-	if (frameWidth > 0) {
-		distortedPixels = distorted.getPixels();
-		
-		if (gotKinectFrame) {
-			for (int x = 0; x < frameWidth; x++) {
-				for (int y = 0; y < frameHeight; y++) {
-					int frameIndex = maskPixelsDetail[y * frameWidth + x] * frameCount / 255 / 255;
-					unsigned char* frame = frames[frameIndex];
-					
-					for (int c = 0; c < 3; c++) {
-						int pixelIndex = y * frameWidth * 3 + x * 3 + c;
-						distortedPixels[pixelIndex] = frame[pixelIndex];
-					}
-				}
-			}
-			gotKinectFrame = false;
-		}
-		
-		ofSetColor(255, 255, 255);
-		
-		mask.setFromPixels(maskPixels, frameWidth, frameHeight, OF_IMAGE_GRAYSCALE);
-		mask.draw(frameWidth/2, 0, frameWidth/2, frameHeight/2);
-		
-		distorted.setFromPixels(distortedPixels, frameWidth, frameHeight, OF_IMAGE_COLOR);
-		distorted.draw(0, frameHeight/2, frameWidth, frameHeight);
-	}
+	mask.setFromPixels(maskOfPixels);
+	mask.draw(0, 0, kinect.width, kinect.height);
 }
 
 void testApp::exit() {
-	clearFrames();
-}
-
-void testApp::readFrames(string filename) {
-	player.loadMovie(filename);
-	player.play();
-	player.setPaused(true);
-	
-	frameCount = player.getTotalNumFrames();
-	frameWidth = player.width;
-	frameHeight = player.height;
-	
-	cout << "Loading frames..." << endl;
-	
-	unsigned char* copyPixels;
-	for (int i = 0; i < frameCount; i++) {
-		cout << i << " of " << frameCount << endl;
-		copyPixels = player.getPixels();
-		unsigned char* framePixels = new unsigned char[frameWidth * frameHeight * 3];
-		for (int i = 0; i < frameWidth * frameHeight * 3; i++) {
-			framePixels[i] = copyPixels[i];
-		}
-		frames.push_back(framePixels);
-		player.nextFrame();
-	}
-	
-	player.closeMovie();
-	
-	distorted.setFromPixels(frames[0], frameWidth, frameHeight, OF_IMAGE_COLOR);
-	
-	// Reset mask.
-	maskPixelsDetail = new unsigned short int[frameWidth * frameHeight];
-	for (int i = 0; i < frameWidth * frameHeight; i++) {
-		maskPixelsDetail[i] = 0;
-	}
-}
-
-void testApp::clearFrames() {
-	for (int i = 0; i < frameCount; i++) {
-		delete[] frames[i];
-	}
-	delete[] maskPixelsDetail;
-		
-	frameCount = 0;
-	frameWidth = 0;
-	frameHeight = 0;
-}
-
-void testApp::writeDistorted() {
-	distorted.setFromPixels(distortedPixels, frameWidth, frameHeight, OF_IMAGE_COLOR);
-	distorted.saveImage("distorted.tga", OF_IMAGE_QUALITY_BEST);
 }
 
 void testApp::keyPressed(int key) {
 }
 
 void testApp::keyReleased(int key) {
-	switch (key) {
-		case 's':
-			writeDistorted();
-			break;
-			
-		case 'x':
-			clearFrames();
-			break;
-	}
 }
 
 void testApp::mouseMoved(int x, int y) {
@@ -155,17 +57,6 @@ void testApp::windowResized(int w, int h) {
 
 void testApp::gotMessage(ofMessage msg) {
 }
+
 void testApp::dragEvent(ofDragInfo dragInfo) {
-	for (int i = 0; i < dragInfo.files.size(); i++) {
-		string filename = dragInfo.files[i];
-		vector<string> tokens = ofSplitString(filename, ".");
-		string extension = tokens[tokens.size() - 1];
-		if (extension == "mov") {
-			clearFrames();
-			readFrames(filename);
-		}
-		else {
-			cout << "Unknown file type: " << extension << endl;
-		}
-	}
 }
--- src/testApp.h
+++ src/testApp.h
@@ -15,11 +15,7 @@ public:
 	void update();
 	void draw();
 	void exit();
-	
-	void clearFrames();
-	void readFrames(string);
-	void writeDistorted();
-	
+		
 	void keyPressed(int key);
 	void keyReleased(int key);
 	void mouseMoved(int x, int y);
@@ -30,22 +26,8 @@ public:
 	void dragEvent(ofDragInfo dragInfo);
 	void gotMessage(ofMessage msg);
 	
-	ofVideoPlayer player;
 	ofxKinect kinect;
 	
-	vector<unsigned char*> frames;
-	ofImage frame;
-	bool gotKinectFrame;
-	
 	ofPixels maskOfPixels;
-	unsigned char* maskPixels;
-	unsigned short int* maskPixelsDetail;
 	ofImage mask;
-	
-	unsigned char* distortedPixels;
-	ofImage distorted;
-	
-	int frameCount;
-	int frameWidth;
-	int frameHeight;
 };
